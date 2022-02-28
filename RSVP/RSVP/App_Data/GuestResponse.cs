4ad737ea6c6c8f6a7f64ebb4df50392dedd2ed49
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2022-02-28 23:41:25 +0300

    Add project files.

---
+++ RSVP/RSVP/App_Data/GuestResponse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class GuestResponse
+{
+    public int GuestResponseId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public bool? WillAttend { get; set; }
+    public DateTime Rdate { get; set; }
+
+    public virtual List<Report> Reports { get; set; }
+
+    public GuestResponse() { }
+    public GuestResponse(string name, string email, string phone, bool? willattend)
+    {
+        Name = name;
+        Email = email;
+        Phone = phone;
+        WillAttend = willattend;
+        Rdate = DateTime.Now;
+        Reports = new List<Report>();
+    }
+
+    public class Report
+    {
+        public int ReportID { get; set; }
+        public string ReportTitle { get; set; }
+        public string Annotation { get; set; }
+        public GuestResponse GResponse { get; set; }
+
+        public Report() { }
+        public Report(string title, string annot)
+        {
+            ReportTitle = title;
+            Annotation = annot;
+        }
+    }
+}
---
+++ RSVP/RSVP/Start.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Start.aspx.cs" Inherits="RSVP.Start" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <linkrel="stylesheet" href="Styles.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <divclass="rek">
+                <h1>Приглашение на семинар</h1>
+                <p>Вы приглашены на наш семинар</p>
+                <p>Подтвердите свое согласие, пройдя регистрацию</p>
+                </div>
+        <divclass="info">
+            Семинар состоится 1 января 2020 года в 7.30
+            </div>
+        </div>
+    </form>
+</body>
+</html>
