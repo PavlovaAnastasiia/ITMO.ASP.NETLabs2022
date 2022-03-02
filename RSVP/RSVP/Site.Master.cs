e547376aaae0ae058c2494b08e7a02cac8c636ca
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2022-03-02 15:44:53 +0300

    .

--- RSVP/RSVP/Site.Master.cs
+++ RSVP/RSVP/Site.Master.cs
@@ -9,9 +9,17 @@ namespace RSVP
 {
     public partial class Site : System.Web.UI.MasterPage
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            long numVisitors = 0;
+            if (Application["Visitors"] != null) 
+            { 
+                numVisitors = 
+                    long.Parse(Application["Visitors"].ToString());
+                VisitorLiteral.Text = "Число посещений: " + numVisitors.ToString();
+            }
+            
         }
     }
 }
