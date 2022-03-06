59b24bc071d3e563eb671fece08bb09382427e47
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2022-03-06 16:10:44 +0300

    .

--- MVC/WebMVCR1/WebMVCR1/Models/Circle.cs
+++ MVC/WebMVCR1/WebMVCR1/Models/Circle.cs
@@ -5,26 +5,73 @@ using System.Web;
 
 namespace WebMVCR1.Models
 {
-    public class Circle
+    //public class Circle
+    //{
+    //    public double St { get; set; }
+    //    public string Name{
+    //        get { return String.Format("\"Окружность с радиусом {0}\"", St); }
+    //    }
+    //    public Circle(double a) 
+    //    { 
+    //        St = a; 
+    //    }
+    //    public double Dlina{
+    //        get{double p = 2 * Math.PI* St; 
+    //            return p;
+    //        }
+    //    }
+    //    public double Area
+    //    { 
+    //        get{ double sq = Math.PI * St * St; 
+    //            return sq; 
+    //        } 
+    //    }
+    //}
+    public class Circle : Shape, IComparable<Circle>
     {
-        public double St { get; set; }
-        public string Name{
-            get { return String.Format("\"Окружность с радиусом {0}\"", St); }
-        }
-        public Circle(double a) 
+        public Circle(double a)
         { 
             St = a; 
         }
-        public double Dlina{
-            get{double p = 2 * Math.PI* St; 
+        override public string Name
+            {
+                get{ return String.Format("\"Окружность с радиусом {0}\"", St); 
+                }
+            }
+        public double Dlina
+        {
+            get
+            {
+                double p = 2 * Math.PI * St;
                 return p;
             }
         }
         public double Area
+        {
+            get
+            {
+                double sq = Math.PI * St * St;
+                return sq;
+            }
+        }
+        public int CompareTo(Circle other) 
         { 
-            get{ double sq = Math.PI * St * St; 
-                return sq; 
-            } 
+            if (this.Area == other.Area) 
+                return 0; 
+            else if(this.Area > other.Area) 
+                return 1; 
+            else return - 1; 
         }
     }
+        public class Shape
+        {
+            public double St { get; set; }
+            virtual public string Name
+            {
+                get
+                {
+                return String.Format("\"Фигура\"");
+                }
+            }
+        }
 }
--- MVC/WebMVCR1/WebMVCR1/Models/Triangle.cs
+++ MVC/WebMVCR1/WebMVCR1/Models/Triangle.cs
@@ -5,9 +5,9 @@ using System.Web;
 
 namespace WebMVCR1.Models
 {
-    public class Triangle
+    public class Triangle: Shape
     {
-        public double Sta { get; set; }
+        //public double Sta { get; set; }
         public double Stb { get; set; }
         public double Stc { get; set; }
 
@@ -28,18 +28,27 @@ namespace WebMVCR1.Models
         {
             get
             {
-                double sq = Math.Sqrt(Perimeter / 2 * (Perimeter / 2 - Sta) *
+                double sq = Math.Sqrt(Perimeter / 2 * (Perimeter / 2 - St) *
                (Perimeter / 2 - Stb) * (Perimeter / 2 - Stc));
                 return sq;
             }
         }
-        public Triangle(double a, double b, double c) 
-        { 
-            Sta = a; 
-            Stb = b; 
-            Stc = c; 
+        public Triangle(double a, double b, double c)
+        {
+            St = a;
+            Stb = b;
+            Stc = c;
         }
-        public double Perimeter => Math.Round(Sta + Stb + Stc);
-        public string Name => $"\"Треугольник со сторонами {Sta}, {Stb} и {Stc}\"";
+        //public double Perimeter => Math.Round(Sta + Stb + Stc);
+        //public string Name => $"\"Треугольник со сторонами {Sta}, {Stb} и {Stc}\"";
+
+        public double Perimeter => Math.Round(St + Stb + Stc);
+        //public string Name => $"\"Треугольник со сторонами {St}, {St} и {St}\"";
+
+        override public string Name
+        {
+            get{ return String.Format("\"Треугольник со сторонами {0}, " +
+                "{1} и {2}\"", St, Stb, Stc); }
+        }
     }
 }
