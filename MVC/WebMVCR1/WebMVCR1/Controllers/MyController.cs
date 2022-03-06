018fabb9ac398f37bd709b414049f41d4dc7ff33
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2022-03-06 15:13:27 +0300

    .

--- MVC/WebMVCR1/WebMVCR1/Controllers/MyController.cs
+++ MVC/WebMVCR1/WebMVCR1/Controllers/MyController.cs
@@ -21,7 +21,11 @@ namespace WebMVCR1.Controllers
             //return Greeting; 
 
             //string res = ExeEnum();
-            string res = ExeStruct(); 
+            //string res = ExeStruct(); 
+            //string res = StudyCsharp.SetStatus(3);
+            //string res = StudyCsharp.ExeSwitch(StudyCsharp.SetStatus(3));
+            //string res = StudyCsharp.GetFunction(0, 9);
+            string res = ExeFactorial(5);
             return res;
         }
 
@@ -40,14 +44,24 @@ namespace WebMVCR1.Controllers
         public string ExeStruct()
         {
             BankAccount goldBankAccount;
-            goldBankAccount.accType = AccountType.Checking; 
-            goldBankAccount.accBal = (decimal)3200.00; 
+            goldBankAccount.accType = AccountType.Checking;
+            goldBankAccount.accBal = (decimal)3200.00;
             goldBankAccount.accNo = 123;
 
             //string res = String.Format("Номер счета {0}, баланс {1}, тип {2}", 
             //    goldBankAccount.accNo, goldBankAccount.accBal, goldBankAccount.accType); 
             string res = String.Format("Информация о банковском счете: {0}", goldBankAccount);
             return res;
+        }
+
+        public string ExeFactorial(int x)
+        {
+            int f; 
+            bool ok = StudyCsharp.Factorial(x, out f);
+            if (ok) 
+                return String.Format("Факториал числа {0} равен {1} ", x, f); 
+            else 
+                return"Невозможно вычислить факториал";
         }
     }
 }
