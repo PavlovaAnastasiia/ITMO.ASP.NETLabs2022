dbce1d1122960cf41ad6fd715b1b67c2fe7e319f
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2022-03-06 18:58:40 +0300

    .

--- MVC/WebMVCR1/WebMVCR1/Controllers/MyController.cs
+++ MVC/WebMVCR1/WebMVCR1/Controllers/MyController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using WebMVCR1.Models;
+//using WebMVCR1.Models;
 
 namespace WebMVCR1.Controllers
 {
@@ -15,8 +16,33 @@ namespace WebMVCR1.Controllers
         //{
         //    return View();
         //}
+        private static PersonRepository db = new PersonRepository();
+        public ViewResult Index() 
+        { 
+            int hour = DateTime.Now.Hour; 
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день"; 
+            ViewData["Mes"] = "хорошего настроения"; 
+            return View(); 
+        }
+        [HttpGet]
+        public ViewResult InputData() 
+        { 
+            return View(); 
+        }
+        [HttpPost] 
+        public ViewResult InputData(Person p) 
+        {
+            db.AddResponse(p);
+            return View("Hello", p); 
+        }
+        public ViewResult OutputData() 
+        { 
+            ViewBag.Pers = db.GetAllResponses; 
+            ViewBag.Count = db.NumberOfPerson; 
+            return View("ListPerson"); 
+        }
 
-        public string Index(string hel)
+        public string Start(string hel)
         {
             string Greeting = ModelClass.ModelHello() + ", " + hel;
             //return Greeting; 
