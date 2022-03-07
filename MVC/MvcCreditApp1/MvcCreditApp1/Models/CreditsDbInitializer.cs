c0c96b51ab6355c9a271b127b9b5cab7812ac819
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2022-03-07 16:04:44 +0300

    .

--- MVC/MvcCreditApp1/MvcCreditApp1/Models/CreditsDbInitializer.cs
+++ MVC/MvcCreditApp1/MvcCreditApp1/Models/CreditsDbInitializer.cs
@@ -11,11 +11,11 @@ namespace MvcCreditApp1.Models
         protected override void Seed(CreditContext context) 
         { 
             context.Credits.Add(new Credit{ Head = "Ипотечный " +
-                "кредит", Period = 10, Sum = 1000000, Procent = 15 }); 
+                " кредит", Period = 10, Sum = 1000000, Procent = 15 }); 
             context.Credits.Add(new Credit{ Head = "Образовательный " +
-                "кредит", Period = 7, Sum = 300000, Procent = 10 }); 
+                " кредит", Period = 7, Sum = 300000, Procent = 10 }); 
             context.Credits.Add(new Credit{ Head = "Потребительский " +
-                "кредит", Period = 5, Sum = 500000, Procent = 19 }); 
+                " кредит", Period = 5, Sum = 500000, Procent = 19 }); 
             base.Seed(context); 
         }
     }
